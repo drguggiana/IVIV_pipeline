44bb2f72902ef58092964f3ad300695e13b4e72a
biojibia <drguggiana@gmail.com>
2020-05-12 12:01:24 +0200

    added plotting of the fitted model

--- potential_scripts/GLM_fit.m
+++ potential_scripts/GLM_fit.m
@@ -37,6 +37,8 @@ combination_coefficients = cell(num_comb,1);
 combination_rmse = zeros(num_comb,1);
 % allocate memory for the labels
 label_cell = cell(num_comb,1);
+% save the models
+model_cell = cell(num_comb,2);
 % for all the combinations
 for combos = 1:num_comb
     % define the sub targets based on the combo
@@ -45,11 +47,13 @@ for combos = 1:num_comb
     idx_vector = idx_vector(idx_vector>0);
     sub_targets = targets(idx_vector);
     % run the regression
-    [combination_results(combos),combination_coefficients{combos},~,~,combination_rmse(combos)] =...
+    [combination_results(combos),combination_coefficients{combos},...
+        ~,~,combination_rmse(combos),model_cell{combos,1},model_cell{combos,2}] =...
         GLM_fitting(str,selection_vector,response,...
         sub_targets,0,true);
     % write the label also
     label_cell{combos} = strjoin(sub_targets,',');
+    
 end
 %% Plot the parameter results
 
@@ -68,6 +72,39 @@ bar(combination_rmse)
 ylabel('RMSE')
 set(gca,'XTick',1:num_comb,'XTickLabels',...
     label_cell,'XTickLabelRotation',45,'TickLabelInterpreter','None')
+%% Plot a desired fit
+
+close all
+% define the target fot
+target_label = 'ang_inL23_Vt,ang_inL4_Vt';
+
+% find the target model
+target_model = model_cell{contains(label_cell,target_label),1};
+target_nan_vector = model_cell{contains(label_cell,target_label),2};
+
+% pull the data
+real_data = vertcat(str(selection_vector).(response));
+% filter the nandata with the vector
+real_data = real_data(target_nan_vector);
+% real_data = (real_data-mean(real_data))./std(real_data);
+
+% get the target model data
+model_data = target_model.Fitted.Response;
+
+% add the mean and std info from the real data
+model_data = model_data.*std(real_data) + mean(real_data);
+
+% calculate a simple linear fit
+lin_mdl = fitlm(model_data,real_data);
+
+figure
+scatter(real_data,model_data,'k','filled')
+hold on
+plot(real_data,predict(lin_mdl,real_data))
+xlabel('Real data')
+ylabel('Predicted data')
+title(strjoin({'Model: ',target_label,'r2:',num2str(lin_mdl.Rsquared.Adjusted)},' '),'Interpreter','None')
+axis equal
 %% Run the regression with shuffles
 
 % get the selection vector (based on the OS cells)
