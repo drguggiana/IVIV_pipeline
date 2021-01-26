c6b74645b270329d2a341323e04ff4db733d2c23
Simon Weiler <simonw@ad.swc.ucl.ac.uk>
2021-01-26 23:20:46 +0000

    implemented sorted correlation

--- old_s/paper_pial_depth.m
+++ old_s/paper_pial_depth.m
@@ -24,24 +24,60 @@ else
 pia_morpho(i)=str_all(morph_id(i)).pia;    
 end
 end
-%% Sholl Analysis
+%% Sholl Analysis APICAL
+for i=1:length(morph_id)
+[s, dd, sd, XP, YP, ZP, iD] = sholl_tree(str_all(morph_id(i)).tr_apical, 20, '-s');
+max_a(i)=max(s);
+temp=dd(find(s==max(s)));
+dis_peaka(i)=temp(1);
+end
+%% Sholl Analysis BASAL
+for i=1:length(morph_id)
+[s, dd, sd, XP, YP, ZP, iD] = sholl_tree(str_all(morph_id(i)).tr_basal, 20, '-s');
+max_b(i)=max(s);
+temp=dd(find(s==max(s)));
+dis_peakb(i)=temp(1);
+end
+%% Correlation with pial depth: APICAL using sorted correlation plot
+ stri={'RD_{max} (µm)','Total Length (µm)','PL_{max} (µm)','BP','BO','BL (µm)','WH','XS','YS','Peak Nr. cross','Dis. peak branch (µm)'}
+sorted_correlation([data_morpho(:,1:9) max_a' dis_peaka'],pia_morpho',stri,'m')
+hold on;xlabel('Correlation with pial depth')
+hold on;set(gca,'FontSize',10)
+%% Correlation with pial depth: BASAL using sorted correlation plot
+stri={'RD_{max} (µm)','Total Length (µm)','PL_{max} (µm)','BP','BO','BL (µm)','WH','XS','YS','BTN','Peak Nr. cross','Dis. peak branch (µm)'}
+sorted_correlation([data_morpho(:,10:19) max_b' dis_peakb'],pia_morpho',stri,'k')
+hold on;xlabel('Correlation with pial depth')
+hold on;set(gca,'FontSize',10);
+%% Sliding winddow analysis APICAL
+[p_wha1 p_wha2] = sliding_window_comp(pia_morpho,data_morpho(:,8));
+%[p_max_a1 p_max_a2] = sliding_window_comp(pia_morpho,max_a);
+%% Sliding winddow analysis BASAL
+[p_blb1 p_blb2] = sliding_window_comp(pia_morpho,data_morpho(:,15));
+[p_max_a1 p_max_a2] = sliding_window_comp(pia_morpho,data_morpho(:,13));
+%% 
 
-
-%% Correlation with pial depth: APICAL
-sorted_correlation(data_morpho(:,1:9),pia_morpho')
-
+%% 
+tr = rescale(pia_morpho);
+fig1=figure;set(gcf,'color','w');set(fig1, 'Position', [200, 200, 500, 200]);
+subplot(1,2,1);
+scatter(data_morpho(:,7)',tr,20,'filled', 'MarkerFaceAlpha',3/8,'MarkerFaceColor','m');
+ %set(gca, 'YDir','reverse');
+ hold on;ref= refline(0,1);set(gca,'FontSize',10);ref.LineStyle='--'; ref.XData=[0 4];xticks([0:1:4]);
+ref.YData=[1 0];yticks([0:0.25:1]);ref.Color='k';ylabel('Relative pial position');xlabel('Width/Height')
+subplot(1,2,2);
+scatter(data_morpho(:,15),tr,20,'filled', 'MarkerFaceAlpha',3/8,'MarkerFaceColor','k');
+ %set(gca, 'YDir','reverse');
+ hold on;ref= refline(0,1);set(gca,'FontSize',10);ref.LineStyle='--'; ref.XData=[30 90];xticks([0:30:90]);
+ref.YData=[0 1];yticks([0:0.25:1]);ref.Color='k';ylabel('Relative pial position');xlabel('Branch length')
 
 %% 
 
-hold on;
-tidx=[];
-tidx=find(P1(end,9:end-1)<0.05);
-corr_basal=R1(end,10:end-1);
-hold on;plot(10:19,corr_basal,'k.')
-hold on;plot(tidx+9,corr_basal(tidx),'k*');
 
-%hold on;line([1 size(data_morpho,2)], [0.05 0.05],'Color','k','LineStyle','--');
+
 %% 
-sliding_window_comp(pia_morpho,data_morpho(:,3))
+figure;scatter(data_morpho(:,15),pia_morpho);
+figure;scatter(data_morpho(:,13),pia_morpho);
+
+
 %% 
        
--- old_s/sorted_correlation.m
+++ old_s/sorted_correlation.m
@@ -1,4 +1,4 @@
-function sorted_correlation(data,sort_data)
+function sorted_correlation(data,sort_data,stri,col)
 
 com=[];com=[data sort_data];
 [R1,P1,RLO1,RUP1]=corrcoef(com,'rows','pairwise');
@@ -12,11 +12,12 @@ up_tree=temp1(k);
 low_tree=temp2(k);
 temp3=[];temp3=P1(end,1:end-1);
 p_tree=temp3(k);
+lab_tree=stri(k);
 
-fig1=figure;set(gcf,'color','w');set(fig1, 'Position', [200, 200, 200, 400])
+fig1=figure;set(gcf,'color','w');set(fig1, 'Position', [200, 200, 400, 600])
 for i=1:length(corr_tree)
-hold on;scatter(corr_tree(i),i,'m','filled')
-hold on;plot([low_tree(i) up_tree(i)],[i i],'-m');
+hold on;scatter(corr_tree(i),i,col,'filled')
+hold on;plot([low_tree(i) up_tree(i)],[i i],col);
 if p_tree(i)<0.001
     text(1.05,i,'***')
 elseif p_tree(i)<0.01
@@ -25,10 +26,14 @@ elseif p_tree(i)<0.05
      text(1.05,i,'*')
 else
 end
-end
-%ylim([0 10]);
 
-hold on;line([0 0], [0 10],'Color','k','LineStyle','--');
+end
+xlim([-1 1.1]);
+ylim([0 length(p_tree)]);
+yticklabels([' ' lab_tree]);
+yticks([0:1:length(p_tree)]);
+hold on;line([0 0], [0 length(corr_tree)],'Color','k','LineStyle','--');
+
 % xlabel('Correlation with pial depth');title('Apical tree');
 % stri={'RDA_{max}(µm)','Total Length (µm)','Peak Nr. crossing','Dis. peak branch (µm)','WHA',...
 %      'RDB_{max}(µm)','Total Length (µm)','Peak Nr. crossing','Dis. peak branch(µm)','WHA'}
